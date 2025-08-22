ec28daa65d938e33cd8ac420cbe61a66c81ad59f
jjlmoya <juanjo1989@gmail.com>
2025-08-23 00:33:05 +0200

    chore: update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel(),
   site: 'https://plantasyflores.online',
   build: {
